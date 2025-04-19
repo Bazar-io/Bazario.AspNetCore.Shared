856c8ea59bfae8d9be62ada3945a4097a0b97dc7
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2025-04-19 20:11:54 +0200

    [BZRIO-68] Add exception middleware (#6)
    https://alexlobchenko00-1742738659749.atlassian.net/browse/BZRIO-68

---
+++ Bazario.AspNetCore.Shared.Web/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Bazario.AspNetCore.Shared.Web.Middleware;
+using Microsoft.AspNetCore.Builder;
+
+namespace Bazario.AspNetCore.Shared.Web.Extensions
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionHandling(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandlingMiddleware>();
+        }
+    }
+}
---
+++ Bazario.AspNetCore.Shared.Web/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bazario.AspNetCore.Shared.Web.Middleware
+{
+    public sealed class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlingMiddleware(
+            RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(
+            HttpContext context,
+            Exception exception)
+        {
+            var problemDetails = new ProblemDetails
+            {
+                Status = StatusCodes.Status500InternalServerError,
+                Title = "Internal Server Error",
+                Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1"
+            };
+
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            var jsonResponse = JsonSerializer.Serialize(problemDetails);
+
+            await context.Response.WriteAsync(jsonResponse);
+        }
+    }
+}
