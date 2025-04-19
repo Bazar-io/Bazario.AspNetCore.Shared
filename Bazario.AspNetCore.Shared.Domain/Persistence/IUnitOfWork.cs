533b8d694879892d5a5b55675f8cfbfb5ccb1eb7
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2025-04-19 17:39:27 +0200

    Add IUnitOfWork

---
+++ Bazario.AspNetCore.Shared.Domain/Persistence/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace Bazario.AspNetCore.Shared.Domain.Persistence
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsync(
+            CancellationToken cancellationToken = default);
+    }
+}
