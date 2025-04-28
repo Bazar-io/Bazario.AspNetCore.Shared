4e91130436535b0aed98dd57794c6bd8a0e69c4a
clowd1e <sashapro712@gmail.com>
2025-04-28 15:56:07 +0200

    Add authorization

---
+++ Bazario.AspNetCore.Shared.Auth/Attributes/HasRoleAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Bazario.AspNetCore.Shared.Auth.Roles;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Bazario.AspNetCore.Shared.Auth.Attributes
+{
+    public sealed class HasRoleAttribute : AuthorizeAttribute
+    {
+        public HasRoleAttribute(Role role)
+            : base(role.ToString())
+        { }
+    }
+}
