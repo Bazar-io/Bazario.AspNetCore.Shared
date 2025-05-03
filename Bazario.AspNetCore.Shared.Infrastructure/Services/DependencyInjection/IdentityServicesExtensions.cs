9124d91b69dc812c1f95df0f789936161f3c72a8
clowd1e <sashapro712@gmail.com>
2025-05-03 16:13:21 +0200

    Provide UserContextService

---
+++ Bazario.AspNetCore.Shared.Infrastructure/Services/DependencyInjection/IdentityServicesExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Bazario.AspNetCore.Shared.Application.Abstractions.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bazario.AspNetCore.Shared.Infrastructure.Services.DependencyInjection
+{
+    public static class IdentityServicesExtensions
+    {
+        public static IServiceCollection AddUserContextServiceWithHttpContextAccessor(
+            this IServiceCollection services)
+        {
+            services.AddHttpContextAccessor();
+            services.AddScoped<IUserContextService, UserContextService>();
+
+            return services;
+        }
+    }
+}
---
+++ Bazario.AspNetCore.Shared.Infrastructure/Services/UserContextService.cs
@@ -0,0 +1,42 @@
+﻿using Bazario.AspNetCore.Shared.Application.Abstractions.Identity;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace Bazario.AspNetCore.Shared.Infrastructure.Services
+{
+    public sealed class UserContextService : IUserContextService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserContextService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public Guid GetAuthenticatedUserId()
+        {
+            var userClaims = _httpContextAccessor.HttpContext?.User;
+
+            if (userClaims?.Identity is null || !userClaims.Identity.IsAuthenticated)
+            {
+                throw new UnauthorizedAccessException("User is not authenticated.");
+            }
+
+            var userIdClaim = userClaims.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (userIdClaim is null)
+            {
+                throw new UnauthorizedAccessException("User ID claim not found.");
+            }
+
+            var userIdString = userIdClaim.Value;
+
+            if (!Guid.TryParse(userIdString, out var userId))
+            {
+                throw new UnauthorizedAccessException("User ID is not a valid GUID.");
+            }
+
+            return userId;
+        }
+    }
+}
