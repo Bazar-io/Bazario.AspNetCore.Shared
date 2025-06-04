4799b83137d23641778068d006aa2ebdb3eb5a79
clowd1e <sashapro712@gmail.com>
2025-06-04 23:17:02 +0200

    Implement DomainEventDispatcher

---
+++ Bazario.AspNetCore.Shared.Infrastructure/DomainEvents/IDomainEventDispatcher.cs
@@ -0,0 +1,11 @@
+﻿using Bazario.AspNetCore.Shared.Abstractions.DomainEvents;
+
+namespace Bazario.AspNetCore.Shared.Infrastructure.DomainEvents
+{
+    public interface IDomainEventDispatcher
+    {
+        Task DispatchAsync(
+            IDomainEvent domainEvent,
+            CancellationToken cancellationToken = default);
+    }
+}
