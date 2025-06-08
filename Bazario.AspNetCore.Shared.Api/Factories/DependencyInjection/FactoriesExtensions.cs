f7681aee96cbe4a829f646978c1fa5b449b07234
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2025-06-09 01:34:16 +0200

    [BZRIO-136] Add Bazario.AspNetCore.Shared.Api package (#21)
    https://alexlobchenko00-1742738659749.atlassian.net/browse/BZRIO-136
    * Add Bazario.AspNetCore.Shared.Api package
    * Add icon for NuGet packages

---
+++ Bazario.AspNetCore.Shared.Api/Factories/DependencyInjection/FactoriesExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Bazario.AspNetCore.Shared.Api.Factories.DependencyInjection
+{
+    public static class FactoriesExtensions
+    {
+        public static IServiceCollection AddProblemDetailsFactory(
+            this IServiceCollection services)
+        {
+            services.AddSingleton<ProblemDetailsFactory>();
+
+            return services;
+        }
+    }
+}
---
+++ Bazario.AspNetCore.Shared.Api/Middleware/DependencyInjection/MiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Bazario.AspNetCore.Shared.Api.Middleware.DependencyInjection
+{
+    public static class ExceptionHandlingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionHandlingMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandlingMiddleware>();
+        }
+    }
+}
