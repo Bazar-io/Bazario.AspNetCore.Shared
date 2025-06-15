165370613708c42d44cabe22ac5c55130b4b967e
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2025-06-15 22:03:30 +0200

    [BZRIO-138] Add UserUpdatedEvent contracts (#22)
    https://alexlobchenko00-1742738659749.atlassian.net/browse/BZRIO-138

---
+++ Bazario.AspNetCore.Shared.Contracts/UserUpdated/UserUpdatedForIdentityServiceEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace Bazario.AspNetCore.Shared.Contracts.UserUpdated
+{
+    public sealed record UserUpdatedForIdentityServiceEvent(
+        Guid UserId,
+        string FirstName,
+        string LastName,
+        string Email,
+        string PhoneNumber);
+}
---
+++ Bazario.AspNetCore.Shared.Contracts/UserUpdated/UserUpdatedForListingServiceEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Bazario.AspNetCore.Shared.Contracts.UserUpdated
+{
+    public sealed record UserUpdatedForListingServiceEvent(
+        Guid UserId,
+        string FirstName,
+        string LastName,
+        string PhoneNumber);
+}
