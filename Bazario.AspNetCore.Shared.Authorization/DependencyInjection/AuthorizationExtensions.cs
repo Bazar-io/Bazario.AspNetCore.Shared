01cb48315205e7bf5f7850c4d3ab9d400ffd6b24
clowd1e <sashapro712@gmail.com>
2025-07-04 17:09:50 +0200

    Refactor authorization policies

--- Bazario.AspNetCore.Shared.Authorization/DependencyInjection/AuthorizationExtensions.cs
+++ Bazario.AspNetCore.Shared.Authorization/DependencyInjection/AuthorizationExtensions.cs
@@ -9,21 +9,56 @@ namespace Bazario.AspNetCore.Shared.Authorization.DependencyInjection
         public static IServiceCollection ConfigureAuthorization(
             this IServiceCollection services)
         {
-            services.AddAuthorizationCore(options => options.ConfigureRoles());
+            services.AddAuthorization(options =>
+            {
+                options.ConfigurePolicy(
+                    permission: Permission.Authenticated,
+                    roles: [Role.User, Role.Admin, Role.Owner]);
+
+                options.ConfigurePolicy(
+                    permission: Permission.User,
+                    roles: [Role.User]);
+
+                options.ConfigurePolicy(
+                    permission: Permission.Admin,
+                    roles: [Role.Admin]);
+
+                options.ConfigurePolicy(
+                    permission: Permission.Owner,
+                    roles: [Role.Owner]);
+
+                options.ConfigurePolicy(
+                    permission: Permission.ChangeOwnData,
+                    roles: [Role.User, Role.Admin]);
+
+                options.ConfigurePolicy(
+                    permission: Permission.ManageUsers,
+                    roles: [Role.Admin, Role.Owner]);
+
+                options.ConfigurePolicy(
+                    permission: Permission.ManageAdmins,
+                    roles: [Role.Owner]);
+
+                options.ConfigurePolicy(
+                    permission: Permission.ManageInternalResources,
+                    roles: [Role.Admin, Role.Owner]);
+            });
 
             return services;
         }
 
-        private static AuthorizationOptions ConfigureRoles(
-            this AuthorizationOptions options)
+        private static AuthorizationOptions ConfigurePolicy(
+            this AuthorizationOptions options,
+            Permission permission,
+            params Role[] roles)
         {
-            foreach (var role in Enum.GetValues<Role>())
-            {
-                options.AddPolicy(role.ToString(), policy =>
+            options.AddPolicy(permission.ToString(),
+                options =>
                 {
-                    policy.RequireRole(role.ToString());
+                    options
+                        .RequireAuthenticatedUser()
+                        .RequireRole(roles.Select(x => x.ToString()));
                 });
-            }
 
             return options;
         }
