2b6af23b1540bfd5116a52a747a41bb7307eab17
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2025-07-05 23:17:55 +0200

    [BZRIO-157] Add code documentation for Bazario.AspNetCore.Shared.Options package (#26)
    https://alexlobchenko00-1742738659749.atlassian.net/browse/BZRIO-157

--- Bazario.AspNetCore.Shared.Options/OptionsExtensions.cs
+++ Bazario.AspNetCore.Shared.Options/OptionsExtensions.cs
@@ -5,6 +5,12 @@ namespace Bazario.AspNetCore.Shared.Options
 {
     public static partial class OptionsExtensions
     {
+        /// <summary>
+        /// Gets options of type <typeparamref name="TOptions"/> from the service provider.
+        /// </summary>
+        /// <typeparam name="TOptions">Options type that implements <see cref="IAppOptions"/>.</typeparam>
+        /// <param name="serviceProvider">Service provider to resolve options from.</param>
+        /// <returns>Options of type <typeparamref name="TOptions"/>.</returns>
         public static TOptions GetOptions<TOptions>(this IServiceProvider serviceProvider)
             where TOptions : class, IAppOptions
         {
