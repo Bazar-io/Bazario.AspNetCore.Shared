0804b281fade2c27358fafbb8c9e713b35b6583f
clowd1e <sashapro712@gmail.com>
2025-07-09 18:50:44 +0200

    Add code documentation

--- Bazario.AspNetCore.Shared.Authorization/Attributes/HasPemissionAttribute.cs
+++ Bazario.AspNetCore.Shared.Authorization/Attributes/HasPemissionAttribute.cs
@@ -2,6 +2,9 @@
 
 namespace Bazario.AspNetCore.Shared.Authorization.Attributes
 {
+    /// <summary>
+    /// Attribute to specify that a user must have a specific permission to access a resource.
+    /// </summary>
     public sealed class HasPemissionAttribute : AuthorizeAttribute
     {
         public HasPemissionAttribute(Permission permission)
