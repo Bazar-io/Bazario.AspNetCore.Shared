54045304bf166a4e663403eab58e1f288f7893df
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2025-07-09 19:02:33 +0200

    [BZRIO-150] Add code documentation for Bazario.AspNetCore.Shared.Api package (#31)
    https://alexlobchenko00-1742738659749.atlassian.net/browse/BZRIO-150

--- Bazario.AspNetCore.Shared.Api/Factories/DependencyInjection/FactoriesExtensions.cs
+++ Bazario.AspNetCore.Shared.Api/Factories/DependencyInjection/FactoriesExtensions.cs
@@ -4,6 +4,10 @@ namespace Bazario.AspNetCore.Shared.Api.Factories.DependencyInjection
 {
     public static class FactoriesExtensions
     {
+        /// <summary>
+        /// Registers the <see cref="ProblemDetailsFactory"/> as a singleton service.
+        /// </summary>
+        /// <param name="services">Service collection to which the factory will be added.</param>
         public static IServiceCollection AddProblemDetailsFactory(
             this IServiceCollection services)
         {
--- Bazario.AspNetCore.Shared.Api/Middleware/DependencyInjection/MiddlewareExtensions.cs
+++ Bazario.AspNetCore.Shared.Api/Middleware/DependencyInjection/MiddlewareExtensions.cs
@@ -4,6 +4,10 @@ namespace Bazario.AspNetCore.Shared.Api.Middleware.DependencyInjection
 {
     public static class ExceptionHandlingMiddlewareExtensions
     {
+        /// <summary>
+        /// Registers the <see cref="ExceptionHandlingMiddleware"/> in the application's request pipeline.
+        /// </summary>
+        /// <param name="builder">Application builder to which the middleware will be added.</param>
         public static IApplicationBuilder UseExceptionHandlingMiddleware(this IApplicationBuilder builder)
         {
             return builder.UseMiddleware<ExceptionHandlingMiddleware>();
