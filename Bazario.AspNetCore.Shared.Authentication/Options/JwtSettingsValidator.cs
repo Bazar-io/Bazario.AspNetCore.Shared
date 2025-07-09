2f217630cfffc4fafaf0880e2af125bbe2b79041
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2025-07-09 18:46:33 +0200

    [BZRIO-153] Add code documentation for Bazario.AspNetCore.Shared.Authentication package (#29)

--- Bazario.AspNetCore.Shared.Authentication/Options/JwtSettingsValidator.cs
+++ Bazario.AspNetCore.Shared.Authentication/Options/JwtSettingsValidator.cs
@@ -2,6 +2,9 @@
 
 namespace Bazario.AspNetCore.Shared.Authentication.Options
 {
+    /// <summary>
+    /// Validator for <see cref="JwtSettings"/> options.
+    /// </summary>
     [OptionsValidator]
     public partial class JwtSettingsValidator
         : IValidateOptions<JwtSettings>
