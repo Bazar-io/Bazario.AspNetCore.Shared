f29da0f10f31e8f654ff6391871674881a23c803
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2025-07-19 14:54:16 +0200

    [BZRIO-183] Add Serilog configuration (#34)
    https://alexlobchenko00-1742738659749.atlassian.net/browse/BZRIO-183
    * Add Serilog DI registration
    * Add RequestLogContextMiddleware

---
+++ Bazario.AspNetCore.Shared.Api/Logging/DependencyInjection/LoggingExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace Bazario.AspNetCore.Shared.Api.Logging.DependencyInjection
+{
+    public static class LoggingExtensions
+    {
+        /// <summary>
+        /// Configures Serilog logging for the application.
+        /// </summary>
+        /// <remarks>
+        /// Call <see cref="SerilogApplicationBuilderExtensions.UseSerilogRequestLogging(IApplicationBuilder, Action{Serilog.AspNetCore.RequestLoggingOptions}?)"/>
+        /// on the <see cref="IApplicationBuilder"/> to enable request logging in the application pipeline.
+        /// </remarks>
+        /// <param name="host">The <see cref="IHostBuilder"/> to configure.</param>
+        public static IHostBuilder ConfigureSerilogFromConfiguration(this IHostBuilder host)
+        {
+            host.UseSerilog((context, configuration) =>
+            {
+                var loggerConfig = configuration.ReadFrom.Configuration(context.Configuration);
+            });
+
+            return host;
+        }
+    }
+}
