69ae0f2b0b5c5e889fdd2f8337aae2bc081ae98d
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2025-07-20 17:10:19 +0200

    [BZRIO-158] Add code documentation for Bazario.AspNetCore.Shared.Results package (#37)
    https://alexlobchenko00-1742738659749.atlassian.net/browse/BZRIO-158

--- Bazario.AspNetCore.Shared.Results/ValidationResultT.cs
+++ Bazario.AspNetCore.Shared.Results/ValidationResultT.cs
@@ -1,5 +1,9 @@
 ﻿namespace Bazario.AspNetCore.Shared.Results
 {
+    /// <summary>
+    /// Represents the generic result of a validation operation that can either succeed or fail.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the value that the operation returns upon success.</typeparam>
     public sealed class ValidationResult<TValue> : Result<TValue>, IValidationResult
     {
         private ValidationResult(Error[] errors) : base(
@@ -10,8 +14,16 @@
             Errors = errors;
         }
 
+        /// <summary>
+        /// Gets a collection of validation errors that occurred during the operation.
+        /// </summary>
         public Error[] Errors { get; }
 
+        /// <summary>
+        /// Creates a <see cref="ValidationResult{TValue}"/> with the specified validation errors.
+        /// </summary>
+        /// <param name="errors">An array of <see cref="Error"/> objects representing the validation errors.</param>
+        /// <returns>A <see cref="ValidationResult{TValue}"/> containing the specified validation errors</returns>
         public static ValidationResult<TValue> WithErrors(Error[] errors) => new(errors);
     }
 }
